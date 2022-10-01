d69462731325d3391a0cec4f185c8282c6de50ec
zpborromeo <zborromeo@ualr.edu>
2022-10-01 12:29:00 -0500

    All TODOs Done - Need to Error Test

--- app/src/main/java/com/ualr/resources/MainActivity.java
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.country_description_text);
+        textView.setText(description);
     }
 
 }
